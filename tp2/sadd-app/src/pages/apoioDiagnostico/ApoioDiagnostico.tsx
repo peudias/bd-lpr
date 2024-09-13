5a5d1903aa662482d8109654f2ab974592bcd67e
pedro dias <pedro.dias@aluno.cefetmg.br>
2024-09-12 21:57:27 -0300

    fix[gerarPdf]: gerando pdf com doenças que aparecem de acordo com os sintomas selecionados

--- tp2/sadd-app/src/pages/apoioDiagnostico/ApoioDiagnostico.tsx
+++ tp2/sadd-app/src/pages/apoioDiagnostico/ApoioDiagnostico.tsx
@@ -7,9 +7,11 @@ import { useEffect, useState } from "react";
 import UseSintoma from "../../modules/sintoma/api/sintomaApi";
 import { SelectVarios, TableLayoutDoenca } from "../../ui/components";
 import { IDoenca } from "../../libs/typings";
+import jsPDF from "jspdf";
+import "jspdf-autotable";
 
 const ApoioDiagnostico = () => {
-  const { loading: loadingDoencas, getDiagnostico } = UseDoenca();
+  const { loading: loadingDoencas, getDiagnostico, logPdf } = UseDoenca();
   const { listAll, loading: loadingSintomas } = UseSintoma();
   const [listaSintomas, setListaSintomas] = useState<string[]>();
   const [response, setResponse] = useState<IDoenca[] | undefined>();
@@ -31,22 +33,6 @@ const ApoioDiagnostico = () => {
 
   const navigate = useNavigate();
 
-  const acoes = (
-    <Box
-      sx={{
-        display: "flex",
-        justifyContent: "flex-start",
-        gap: 2,
-        marginTop: "16px",
-        marginBottom: "24px",
-      }}
-    >
-      <Button variant="contained" color="primary" onClick={onSubmit}>
-        Verificar doenças
-      </Button>
-    </Box>
-  );
-
   const searchsOption = (
     <Box sx={{ maxWidth: "70%", width: "100%" }}>
       <SelectVarios
@@ -58,6 +44,31 @@ const ApoioDiagnostico = () => {
     </Box>
   );
 
+  const acoes = (
+    <Box
+      sx={{
+        display: "flex",
+        flexDirection: "row",
+        gap: 2,
+        marginTop: "16px",
+        marginBottom: "24px",
+        width: "100%",
+      }}
+    >
+      <Button variant="contained" color="primary" onClick={onSubmit}>
+        Verificar doenças
+      </Button>
+      <Button
+        variant="contained"
+        color="primary"
+        onClick={() => gerarRelatorioPDF(response)}
+        disabled={!response}
+      >
+        Gerar Relatório PDF
+      </Button>
+    </Box>
+  );
+
   return (
     <PageLayout
       titleComponent={
@@ -66,9 +77,9 @@ const ApoioDiagnostico = () => {
       onBack={() => {
         navigate(-1);
       }}
-      searchs={searchsOption}
-      actions={acoes}
     >
+      {searchsOption}
+      {acoes}
       {loadingDoencas || loadingSintomas ? (
         <LoadingContainer>
           <CircularProgress />
@@ -90,8 +101,77 @@ const ApoioDiagnostico = () => {
     try {
       const responseBack = await getDiagnostico(sintomasSelecionados);
       setResponse(responseBack);
-      console.log(responseBack);
-    } catch (error) {}
+    } catch (error) {
+      console.error("Erro ao buscar diagnóstico:", error);
+    }
+  }
+
+  function gerarRelatorioPDF(doencas: IDoenca[] | undefined) {
+    if (!doencas) return;
+
+    const doc = new jsPDF();
+    const agora = new Date();
+    const dataAtual = agora.toLocaleDateString("pt-BR", {
+      day: "2-digit",
+      month: "2-digit",
+      year: "numeric",
+    });
+    const horaAtual = agora.toLocaleTimeString("pt-BR", {
+      hour: "2-digit",
+      minute: "2-digit",
+    });
+
+    doc.text(
+      `Relatório de Diagnóstico - Horário da impressão: ${dataAtual} às ${horaAtual}`,
+      20,
+      20
+    );
+
+    const body = doencas.map((doenca) => [
+      doenca.nomes_tecnicos,
+      doenca.CID,
+      doenca.patogeno.nome_cientifico,
+      doenca.patogeno.tipo,
+      doenca.nomes_populares?.join(", ") || "Nenhum",
+      doenca.sintomas?.map((sintoma) => sintoma.nome).join(", ") || "Nenhum",
+      doenca.sintomas
+        ?.map((sintoma) => sintoma.nivel_de_ocorrencia)
+        .join(", ") || "Nenhum",
+    ]);
+
+    doc.autoTable({
+      startY: 30,
+      head: [
+        [
+          "Nome Técnico",
+          "CID",
+          "Patógeno",
+          "Tipo de Patógeno",
+          "Nomes Populares",
+          "Sintomas",
+          "Nível de Ocorrência",
+        ],
+      ],
+      body: body,
+      headStyles: {
+        fillColor: [70, 129, 139],
+      },
+    });
+
+    const dataHoraFormatada = `${agora
+      .toLocaleDateString("pt-BR")
+      .replace(/\//g, "-")}_${agora.getHours()}-${agora.getMinutes()}`;
+
+    doc.save(`relatorio_diagnostico_${dataHoraFormatada}.pdf`);
+    logPdfDoenca();
+  }
+
+  async function logPdfDoenca() {
+    try {
+      await logPdf();
+    } catch (error) {
+      console.error("Erro ao registrar log de PDF:", error);
+    }
   }
 };
 
